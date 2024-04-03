e9c444eac67c08b45fb56f9840e375f6a769e6a3
unknown <fishermujahid@gmail.com>
2024-04-03 14:41:11 +0200

    done

--- API/code.js
+++ API/code.js
@@ -1,11 +1,33 @@
 const express = require("express");
 const app = express();
+require("./config/route");
+require("./models/connectDB");
+const excel = require("exceljs");
 const bodyParser = require("body-parser");
+const User = require("./models/connectDB");
 const PORT = process.env.PORT || 4000;
 
 app.use(bodyParser.json());
 app.use(express.static("./static"));
 app.use(express.json())
+
+app.post("/register", async (req, res) => {
+  try {
+    const { firstName, lastName } = req.body;
+
+    const newUser = new User({
+      firstName: firstName,
+      lastName: lastName,
+    });
+
+    await newUser.save();
+
+    res.status(201).json({ message: "User registered successfully" });
+  } catch (error) {
+    console.error("Error registering user:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
