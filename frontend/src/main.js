80cc479ce61e1fe0be5fd016a2fadae34a2f82e8
unknown <fishermujahid@gmail.com>
2024-04-04 12:41:03 +0200

    check

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,5 +3,8 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import CSS from "./assets/style.css"
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App).use(store).use(router).use(CSS).mount('#app')
--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,14 +1,53 @@
-import { createStore } from 'vuex'
+import { createStore } from "vuex";
+import axios from "axios";
+import Swal from "sweetalert2";
 
 export default createStore({
   state: {
-  },
-  getters: {
+    firstName: "",
+    lastName: "",
   },
   mutations: {
+    setFirstName(state, firstName) {
+      state.firstName = firstName;
+    },
+    setLastName(state, lastName) {
+      state.lastName = lastName;
+    },
+    clearForm(state) {
+      state.firstName = "";
+      state.lastName = "";
+    },
   },
   actions: {
+    async registerUser({ state, commit }) {
+      try {
+        const response = await axios.post("https://prototype-api-iakc.onrender.com/register", {
+          firstName: state.firstName,
+          lastName: state.lastName,
+        });
+        if (response.status === 201) {
+          commit("clearForm");
+          Swal.fire({
+            icon: "success",
+            title: "Success",
+            text: response.data.message,
+          });
+        } else {
+          Swal.fire({
+            icon: "error",
+            title: "Error",
+            text: response.data.error,
+          });
+        }
+      } catch (error) {
+        console.error("Error registering user:", error);
+        Swal.fire({
+          icon: "error",
+          title: "Error",
+          text: "Internal server error",
+        });
+      }
+    },
   },
-  modules: {
-  }
-})
+});
